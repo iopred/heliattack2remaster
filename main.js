9aa11e73f8b9bc52a77fd37c6420cf7366571f5a
Kit Rhodes <iopred@gmail.com>
2024-11-10 18:23:29 +1000

    Basic weapon system, most weapons half functioning :)

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ import * as THREE from 'three';
 import WebGL from 'three/addons/capabilities/WebGL.js';
 import { FontLoader } from 'three/addons/loaders/FontLoader.js';
 import { TextGeometry } from 'three/addons/geometries/TextGeometry.js';
+import { Vector2 } from 'three';
+import { Vector3 } from 'three';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 2000);
@@ -104,7 +106,12 @@ const keyIsPressed = {
 };
 
 const raycaster = new THREE.Raycaster();
-const mouse = new THREE.Vector2()
+const mouse = {
+    x: 0,
+    y: 0,
+    down: false,
+    wheel: 0,
+}
 
 
 function onDocumentMouseMove(event) {
@@ -113,6 +120,30 @@ function onDocumentMouseMove(event) {
     mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
 }
 
+
+
+const planeZ = new THREE.Plane(new THREE.Vector3(0, 0, 1), 0);
+
+function calculateAngleToMouse(object, mouse) {
+    // Update raycaster with the mouse position in NDC
+    raycaster.setFromCamera(mouse, camera);
+
+    const intersectPoint = new THREE.Vector3();
+    // Calculate the point where the ray intersects with the plane z = 0
+    raycaster.ray.intersectPlane(planeZ, intersectPoint);
+
+    // Get object position in world coordinates
+    const objectPosition = new THREE.Vector3();
+    object.getWorldPosition(objectPosition);
+
+    // Calculate the angle
+    const angle = Math.atan2(
+        intersectPoint.y - objectPosition.y,
+        intersectPoint.x - objectPosition.x
+    );
+
+    return angle;
+}
 
 function manageRaycasterIntersections(scene, camera, vector) {
     camera.updateMatrixWorld();
@@ -125,9 +156,23 @@ function manageRaycasterIntersections(scene, camera, vector) {
 }
 
 function onMouseDown(event){
-    console.log("mouse position: (" + mouse.x + ", "+ mouse.y + ")");
+    //console.log("mouse position: (" + mouse.x + ", "+ mouse.y + ")");
     manageRaycasterIntersections(scene, camera, mouse);
- }
+    mouse.down = true;
+}
+
+
+function onMouseUp(event){
+    mouse.down = false;
+}
+
+function onMouseWheel(event){
+    if (event.deltaY < 0) {
+        mouse.wheel = -1;
+    } else if (event.deltaY > 0) {
+        mouse.wheel = 1;
+    }
+};
 
 function init() {
     window.addEventListener('resize', onWindowResize);
@@ -135,6 +180,8 @@ function init() {
     document.addEventListener('mousemove', onDocumentMouseMove, false);
     window.addEventListener('resize', onWindowResize, false);
     document.addEventListener('mousedown', onMouseDown, false);
+    document.addEventListener('mouseup', onMouseUp, false);
+    window.addEventListener('wheel', onMouseWheel, false);
 
     window.addEventListener('keydown', (e) => { keyIsPressed[e.key] = true; });
     window.addEventListener('keyup', (e) => { keyIsPressed[e.key] = false; });
@@ -187,7 +234,7 @@ const bg1 = [[[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0],[0, 0],[0, 
 
 
 class Weapon {
-    constructor(name, textureUrl, origin /* {THREE.Vector2} */, barrel /* {THREE.Vector2} */, reloadTime, bulletSpeed, damage) {
+    constructor(name, textureUrl, origin /* {THREE.Vector2} */, barrel /* {THREE.Vector2} */, reloadTime, bulletSpeed, damage, bulletTextureUrl) {
         this.name = name;
         this.textureUrl = textureUrl;
         this.origin = origin;
@@ -195,9 +242,26 @@ class Weapon {
         this.reloadTime = reloadTime;
         this.bulletSpeed = bulletSpeed;
         this.damage = damage;
+        this.bulletTextureUrl = bulletTextureUrl;
+        this.reloading = Number.POSITIVE_INFINITY;
+        this.bullets = 1;
+        this.bulletsSpread = 0;
+        this.spread = 0;
     }
 
-    init(texture) {
+    setSpread(spread) {
+        this.spread = spread;
+        return this;
+    }
+
+    setBullets(bullets, bulletsSpread) {
+        this.bullets = bullets;
+        this.bulletsSpread = bulletsSpread || 0;
+        return this;
+    }
+
+    init(textures) {
+        const texture = textures[this.textureUrl];
         this.geometry = new THREE.PlaneGeometry(texture.image.width, texture.image.height);
         this.material = new THREE.MeshBasicMaterial({
             map: texture,
@@ -205,24 +269,38 @@ class Weapon {
         });
         this.geometry.translate(texture.image.width / 2, -texture.image.height / 2, 0); 
         this.mesh = new THREE.Mesh(this.geometry, this.material);
+        if (this.bulletTextureUrl) {
+            this.bulletTexture = textures[this.bulletTextureUrl];
+        }
+    }
+
+    createBullet(createBulletFunc) {
+        let rot = 0;
+        if (this.bullets > 1) {
+            rot = -this.bulletsSpread * this.bullets / 2;
+        }
+        for (var i = 0; i < this.bullets; i++) {
+            createBulletFunc(this, rot + THREE.MathUtils.randFloat(-this.spread, this.spread));
+            rot += this.bulletsSpread;
+        }
     }
 }
 
 const weapons = [
-    new Weapon("Machine Gun", 'images/weapons/machinegun.png', new THREE.Vector2(5, 12), new THREE.Vector2(23, -7.5), 5, 8, 10),
-    new Weapon("Akimbo Mac10's", 'images/weapons/mac10s.png', new THREE.Vector2(-2, 21), new THREE.Vector2(28, -8.5), 4, 8, 9),
-    new Weapon("Shotgun", 'images/weapons/shotgun.png', new THREE.Vector2(5, 12), new THREE.Vector2(30, -7), 25, 8, 15),
-    new Weapon("Shotgun Rockets", 'images/weapons/shotgunrockets.png', new THREE.Vector2(7, 19), new THREE.Vector2(34, -8), 40, 7, 40),
-    new Weapon("Grenade Launcher", 'images/weapons/grenadelauncher.png', new THREE.Vector2(13, 18), new THREE.Vector2(29, -7), 30, 15, 75),
-    new Weapon("RPG", 'images/weapons/rpg.png', new THREE.Vector2(18, 20), new THREE.Vector2(32, -7), 40, 4, 75),
-    new Weapon("Rocket Launcher", 'images/weapons/rocketlauncher.png', new THREE.Vector2(19, 23), new THREE.Vector2(25, -9.5), 50, 7, 100),
-    new Weapon("Seeker Launcher", 'images/weapons/seekerlauncher.png', new THREE.Vector2(24, 28), new THREE.Vector2(24, -9.5), 55, 7, 100),
-    new Weapon("Flame Thrower", 'images/weapons/flamethrower.png', new THREE.Vector2(9, 16), new THREE.Vector2(29, -7), 1, 8, 2),
-    new Weapon("Fire Mines", 'images/weapons/mine.png', new THREE.Vector2(-9, 15), new THREE.Vector2(20, -5.5), 100, 3, 5),
-    new Weapon("A-Bomb Launcher", 'images/weapons/abomb.png', new THREE.Vector2(22, 30), new THREE.Vector2(36, -13), 150, 3, 300),
-    new Weapon("Rail Gun", 'images/weapons/railgun.png', new THREE.Vector2(23, 27), new THREE.Vector2(32, -8), 75, 20, 150),
-    new Weapon("Grapple Cannon", 'images/weapons/grapplecannon.png', new THREE.Vector2(18, 23), new THREE.Vector2(33, -11), 250, 20, 300),
-    new Weapon("Shoulder Cannon", 'images/weapons/shouldercannon.png', new THREE.Vector2(0, 0), new THREE.Vector2(16, 0), 100, 20, 300),
+    new Weapon("Machine Gun", 'images/weapons/machinegun.png', new THREE.Vector2(5, 12), new THREE.Vector2(23, -7.5), 5, 8, 10).setSpread(2),
+    new Weapon("Akimbo Mac10's", 'images/weapons/mac10s.png', new THREE.Vector2(-2, 21), new THREE.Vector2(28, -8.5), 4, 8, 9).setSpread(8).setBullets(2),
+    new Weapon("Shotgun", 'images/weapons/shotgun.png', new THREE.Vector2(5, 12), new THREE.Vector2(30, -7), 25, 8, 15).setBullets(5, 5),
+    new Weapon("Shotgun Rockets", 'images/weapons/shotgunrockets.png', new THREE.Vector2(7, 19), new THREE.Vector2(34, -8), 40, 7, 40, 'images/shotgunrocketbullet.png').setBullets(3, 10),
+    new Weapon("Grenade Launcher", 'images/weapons/grenadelauncher.png', new THREE.Vector2(13, 18), new THREE.Vector2(29, -7), 30, 15, 75, 'images/grenade.png'),
+    new Weapon("RPG", 'images/weapons/rpg.png', new THREE.Vector2(18, 20), new THREE.Vector2(32, -7), 40, 4, 75, 'images/rpgbullet.png'),
+    new Weapon("Rocket Launcher", 'images/weapons/rocketlauncher.png', new THREE.Vector2(19, 23), new THREE.Vector2(25, -9.5), 50, 7, 100, 'images/rocketbullet.png'),
+    new Weapon("Seeker Launcher", 'images/weapons/seekerlauncher.png', new THREE.Vector2(24, 28), new THREE.Vector2(24, -9.5), 55, 7, 100, 'images/seekerbullet.png'),
+    new Weapon("Flame Thrower", 'images/weapons/flamethrower.png', new THREE.Vector2(9, 16), new THREE.Vector2(29, -7), 1, 8, 2, 'images/flamebullet.png'),
+    new Weapon("Fire Mines", 'images/weapons/mine.png', new THREE.Vector2(-9, 15), new THREE.Vector2(20, -5.5), 100, 3, 5, 'images/minebullet.png'),
+    new Weapon("A-Bomb Launcher", 'images/weapons/abomb.png', new THREE.Vector2(22, 30), new THREE.Vector2(36, -13), 150, 3, 300, 'images/abombbullet.png'),
+    new Weapon("Rail Gun", 'images/weapons/railgun.png', new THREE.Vector2(23, 27), new THREE.Vector2(32, -8), 75, 20, 150, 'images/rail.png'),
+    new Weapon("Grapple Cannon", 'images/weapons/grapplecannon.png', new THREE.Vector2(18, 23), new THREE.Vector2(33, -11), 250, 20, 300, 'images/grapplebullet.png'),
+    new Weapon("Shoulder Cannon", 'images/weapons/shouldercannon.png', new THREE.Vector2(0, 0), new THREE.Vector2(16, 0), 100, 20, 300, 'images/shouldercannon.png'),
 ];
 
 // Helper to load a texture as a promise
@@ -249,13 +327,17 @@ async function loadAssets() {
         loadTexture(loader, textureMap, 'images/tilesheet.png'),
         loadTexture(loader, textureMap, 'images/player.png'),
         loadTexture(loader, textureMap, 'images/bg.png'),
+        loadTexture(loader, textureMap, 'images/bullet.png'),
     ];
     for (const weapon of weapons) {
         textures.push(loadTexture(loader, textureMap, weapon.textureUrl));
+        if (weapon.bulletTextureUrl) {
+            textures.push(loadTexture(loader, textureMap, weapon.bulletTextureUrl));
+        }
     }
     await Promise.all(textures);
     for (const weapon of weapons) {
-        weapon.init(textureMap[weapon.textureUrl]);
+        weapon.init(textureMap);
     }
     return textureMap;
 }
@@ -276,21 +358,21 @@ function setUV(geometry, index, size, width, height) {
 }
 
 // AABB collision detection function
-function checkTileCollisions(entity, delta, tilemap, tileSize) {
-    const xCol = resolveAxisCollision(entity, delta, 'x', tilemap, tileSize);
-    const yCol = resolveAxisCollision(entity, delta, 'y', tilemap, tileSize);
+function checkTileCollisions(entity, timeScale, tilemap, tileSize) {
+    const xCol = resolveAxisCollision(entity, timeScale, 'x', tilemap, tileSize);
+    const yCol = resolveAxisCollision(entity, timeScale, 'y', tilemap, tileSize);
 
     return [xCol, yCol];
 }
 
 // Helper function to resolve collision along a single axis
-function resolveAxisCollision(entity, delta, axis, tilemap, tileSize) {
+function resolveAxisCollision(entity, timeScale, axis, tilemap, tileSize) {
     if (entity.velocity[axis] == 0) {
         return false;
     }
 
     let point = entity.position[axis] + (entity.velocity[axis] > 0 ? entity.bounds.max[axis] : entity.bounds.min[axis]);
-    const tilePos = Math.floor((point + entity.velocity[axis] * delta) / tileSize);
+    const tilePos = Math.floor((point + entity.velocity[axis] * timeScale) / tileSize);
 
     const otherAxis = axis === 'x' ? 'y' : 'x';
     const startOther = Math.floor((entity.position[otherAxis] + entity.bounds.min[otherAxis]) / tileSize);
@@ -319,7 +401,7 @@ function resolveAxisCollision(entity, delta, axis, tilemap, tileSize) {
         }
     
 
-    entity.position[axis] += entity.velocity[axis] * delta;
+    entity.position[axis] += entity.velocity[axis] * timeScale;
     return false;
 }
 
@@ -345,6 +427,10 @@ function initScene(textures) {
     const tilesheet = textures['images/tilesheet.png'];
     const playerTexture = textures['images/player.png'];
     const bgTexture = textures['images/bg.png'];
+    const bulletTexture = textures['images/bullet.png'];
+
+    
+    const zero = new THREE.Vector3();
 
     const tileSize = 50;
     const sheetWidth = tilesheet.image.width;
@@ -408,6 +494,10 @@ function initScene(textures) {
     const mapLayer = buildMap(map1);
     world.add(mapLayer);
 
+    
+    const mapBox = new THREE.Box3(new THREE.Vector3(0, -mapHeight, 0), new THREE.Vector3(mapWidth, 0, 0));
+    mapBox.expandByScalar(tileSize);
+
     cube.position.x = 400;
     cube.position.y = -400;
 
@@ -417,6 +507,24 @@ function initScene(textures) {
     const playerSize = 55;
     const playerWidth = playerTexture.image.width;
     const playerHeight = playerTexture.image.height;
+
+    let timeScale = 1;
+    const player = {
+        tick: 0,
+        position: new THREE.Vector2(tileSize / 2, 47),
+        velocity: new THREE.Vector2(),
+        bounds: {
+            min: new THREE.Vector2(-12, -47),
+            max: new THREE.Vector2(8, -4),
+        },
+        jumps: 2,
+        jumping: 0,
+        weapon: 0,
+        hand: new THREE.Vector2(0, 19),
+        aim: 0,
+    }
+    const playerBullets = [];
+    const enemyBullets = [];
 
     const playerGroup = new THREE.Group();
 
@@ -430,7 +538,14 @@ function initScene(textures) {
     playerBody.position.set(0, playerSize/2, -1);
     setUV(playerGeometry, 1, playerSize, playerWidth, playerHeight);
     playerGroup.add(playerBody);
-    playerGroup.add(weapons[0].mesh);
+
+    const playerWeapon = new THREE.Object3D()
+    playerWeapon.add(weapons[0].mesh);
+
+    playerWeapon.position.set(player.hand.x, player.hand.y, 0);
+    playerWeapon.rotation.z = -Math.PI/4;
+
+    playerGroup.add(playerWeapon);
 
     // createBlueLine(0, 19, playerGroup);
 
@@ -438,27 +553,11 @@ function initScene(textures) {
 
     scene.add(world);
     
-
-    let timeScale = 1;
-    let player = {
-        tick: 0,
-        position: new THREE.Vector2(tileSize / 2, 47),
-        velocity: new THREE.Vector2(),
-        bounds: {
-            min: new THREE.Vector2(-12, -47),
-            max: new THREE.Vector2(8, -4),
-        },
-        jumps: 2,
-        jumping: 0,
-        weapon: 0,
-        hand: new THREE.Vector2(0, 19),
-    }
-
     function selectWeapon(weaponIndex) {
-        playerGroup.remove(weapons[player.weapon].mesh);
+        playerWeapon.remove(weapons[player.weapon].mesh);
         player.weapon = weaponIndex;
         const weapon = weapons[weaponIndex];
-        playerGroup.add(weapon.mesh);
+        playerWeapon.add(weapon.mesh);
 
         updateWeaponPosition();
     }
@@ -470,16 +569,17 @@ function initScene(textures) {
 
     function updateWeaponPosition() {
         const weapon = weapons[player.weapon];
-        weapon.mesh.position.x = player.hand.x - weapon.origin.x;
-        weapon.mesh.position.y = player.hand.y + weapon.origin.y;
+        weapon.mesh.position.x = -weapon.origin.x;
+        weapon.mesh.position.y = weapon.origin.y;
     }
 
     updatePlayerGroup();
-    selectWeapon(6);
+    selectWeapon(4);
+    weapons[player.weapon].reloading = Number.POSITIVE_INFINITY;
 
     // Player movement logic
-    function updatePlayerPosition(delta) {
-        player.tick += delta;
+    function updatePlayer(timeScale, delta) {
+        player.tick += timeScale;
         while (player.tick > 1) {
             player.tick -= 1;
         
@@ -513,7 +613,7 @@ function initScene(textures) {
             player.velocity.y = Math.min(player.velocity.y + 0.5, tileSize);
         }
         
-        let [xCol, yCol] = checkTileCollisions(player, delta, map1, tileSize);
+        let [xCol, yCol] = checkTileCollisions(player, timeScale, map1, tileSize);
 
         if (player.position.x + player.bounds.min.x <= 0) {
             player.position.x = -player.bounds.min.x;
@@ -537,11 +637,83 @@ function initScene(textures) {
             player.velocity.y = 0;
 
         }
+        
+        player.aim = calculateAngleToMouse(playerWeapon, mouse);
+        if (player.aim > Math.PI/2 || player.aim < -Math.PI/2) {
+            playerWeapon.scale.x = -1;
+            playerWeapon.rotation.z = player.aim + Math.PI;
+        } else {
+            playerWeapon.scale.x = 1;
+            playerWeapon.rotation.z = player.aim
+        }
+
+        if (mouse.wheel != 0) {
+            selectWeapon(THREE.MathUtils.euclideanModulo(player.weapon - mouse.wheel, weapons.length));
+            mouse.wheel = 0;
+        }
+
+        const weapon = weapons[player.weapon];
+        weapon.reloading++;
+        if (mouse.down) {
+            if (weapon.reloading > weapon.reloadTime) {
+                weapon.createBullet(createBullet);
+                weapon.reloading = 0;
+            }
+        }
 
         updatePlayerGroup();
     }
 
-    function updateCameraPosition(delta) {
+    function rotateAroundPivot(point, pivot, angle, flip) {
+        const cosAngle = Math.cos(angle);
+        const sinAngle = Math.sin(angle);
+    
+        // Step 1: Translate point to the origin (relative to the pivot)
+        const dx = point.x - pivot.x;
+        const dy = (flip ? -point.y: point.y) - pivot.y;
+        
+        // Step 2: Rotate the point
+        const rotatedX = dx * cosAngle - dy * sinAngle;
+        const rotatedY = dx * sinAngle + dy * cosAngle;
+
+        // Step 3: Translate back to the pivot's position
+        return {
+            x: rotatedX + pivot.x,
+            y: rotatedY + pivot.y,
+            z: 0,
+        };
+    }
+
+    function createBullet(weapon, rotation) {
+        const pivot = new THREE.Vector3();
+        playerWeapon.getWorldPosition(pivot);
+
+        const texture = weapon.bulletTexture || bulletTexture;
+
+        const geometry = new THREE.PlaneGeometry(texture.image.width, texture.image.height);
+        const material = new THREE.MeshBasicMaterial({ 
+            map: texture,
+            transparent: true
+        });
+
+        const direction = player.aim + rotation * Math.PI/180
+        
+        const mesh = new THREE.Mesh(geometry, material);
+        mesh.rotation.z = direction;
+
+        mesh.position.copy(pivot.add(rotateAroundPivot(weapon.barrel, zero, player.aim, !(player.aim > Math.PI/2 || player.aim < -Math.PI/2))));
+
+        scene.add(mesh);
+        playerBullets.push({
+            velocity: new THREE.Vector3(
+                weapon.bulletSpeed * Math.cos(direction),
+                weapon.bulletSpeed * Math.sin(direction),
+                0),
+            object: mesh,
+        });
+    }
+
+    function updateCameraPosition(timeScale, delta) {
         let worldPos = new THREE.Vector3();
         playerGroup.getWorldPosition(worldPos);
 
@@ -552,6 +724,18 @@ function initScene(textures) {
         camera.position.y = THREE.MathUtils.damp(camera.position.y, Math.min(Math.max(Math.round(worldPos.y), -(mapHeight - visibleHeight)), -visibleHeight), 10, delta);
     }
 
+    function updateBullets(timeScale, delta) {
+        for (let i = playerBullets.length - 1; i >= 0; i--) {
+            const bullet = playerBullets[i];
+            bullet.object.position.add(bullet.velocity);
+            //bullet.object.rotation.z += 5 * Math.PI/180;
+            if (!mapBox.containsPoint(bullet.object.position)) {
+                playerBullets.splice(i, 1);
+                scene.remove(bullet.object);
+            }
+        }
+    }
+
     // Game loop
     gameLoop = function () {
         const delta = clock.getDelta();
@@ -559,8 +743,9 @@ function initScene(textures) {
         accumulator += delta;
 
         if (accumulator > 1/60) {
-            updatePlayerPosition(timeScale);
-            updateCameraPosition(timeScale);
+            updatePlayer(timeScale, delta);
+            updateCameraPosition(timeScale, delta);
+            updateBullets(timeScale, delta);
             accumulator %= 1/60;
         };
         
