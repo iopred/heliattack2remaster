ff96ff27e73ae41bcd812f884a419105f47ebd6f
Kit Rhodes <iopred@gmail.com>
2024-12-28 17:22:49 +1000

    Fix up build script.. Wtf

--- build.sh
+++ build.sh
@@ -2,5 +2,13 @@
 
 rm -rf dist
 npx vite build
+
+cp images/ui/*.png dist/images/ui/
+mkdir dist/images/ui/menu
+cp images/ui/menu/*.glb dist/images/ui/menu/
+cp sounds/game/*.wav dist/sounds/
+mkdir dist/scc
+cp scc/squarecircleco.glb dist/scc/
+
 cd dist
 zip -r dist.zip *
